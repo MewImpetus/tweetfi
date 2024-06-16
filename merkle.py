4783234d1da2168741fb7b8911bdbb895454dcdb
geb <853934146@qq.com>
2024-06-16 21:43:25 +0800

    feat: update contract

---
+++ merkle.py
@@ -0,0 +1,70 @@
+import hashlib
+
+
+def hash_data(data):
+    # 创建 sha256 哈希对象
+    hasher = hashlib.sha256()
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    hasher.update(data)
+    digest = hasher.digest()
+    full_hash = int.from_bytes(digest, 'big')
+    truncated_hash = full_hash >> 128
+
+    return str(truncated_hash)
+
+
+
+
+class MerkleTree:
+    def __init__(self, data):
+        self.leaves = [hash_data(item) for item in data]
+        self.tree = []
+        self.build_tree()
+
+    def build_tree(self):
+        current_layer = self.leaves
+        while len(current_layer) > 1:
+            next_layer = []
+            for i in range(0, len(current_layer), 2):
+                left = current_layer[i]
+                right = current_layer[i + 1] if i + 1 < len(current_layer) else left
+                next_layer.append(hash_data(left + right))
+            self.tree.append(current_layer)
+            current_layer = next_layer
+        self.tree.append(current_layer)  # Root
+
+    def get_proof(self, index):
+        proof = []
+        layer_index = 0
+        while layer_index < len(self.tree) - 1:
+            layer = self.tree[layer_index]
+            is_right_node = index % 2
+            sibling_index = index + 1 if is_right_node == 0 else index - 1
+            if sibling_index < len(layer):
+                proof.append((layer[sibling_index], 'right' if is_right_node == 0 else 'left'))
+            index = index // 2
+            layer_index += 1
+        return proof
+
+    def verify_proof(self, proof, target_hash, root_hash):
+        current_hash = target_hash
+        for sibling_hash, position in proof:
+            if position == 'left':
+                current_hash = hash_data(sibling_hash + current_hash)
+            else:
+                current_hash = hash_data(current_hash + sibling_hash)
+        return current_hash == root_hash
+
+
+# Example Usage
+data_blocks = ['123EQAX21A4fIw7hX1jmRjvJT0DX7H_FUItj2duCBWtK4ayEiC_10000000000000', '1EQBGhqLAZseEqRXz4ByFPTGV7SVMlI4hrbs-Sps_Xzx01x8G990000000000000', 'block3', 'block4']
+merkle_tree = MerkleTree(data_blocks)
+target_index = 1
+proof = merkle_tree.get_proof(target_index)
+verified = merkle_tree.verify_proof(proof, merkle_tree.leaves[target_index], merkle_tree.tree[-1][0])
+
+print("root:", merkle_tree.tree[-1][0])
+print("Proof:", proof)
+print("Verified:", verified)
+print("Target hash:", merkle_tree.leaves[target_index])
--- tests/TweetFi.spec.ts
+++ tests/TweetFi.spec.ts
@@ -1,6 +1,8 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano } from '@ton/core';
+import { toNano, Address, Cell, beginCell } from '@ton/core';
 import { TweetFi } from '../wrappers/TweetFi';
+import { TransactionValidator } from '../build/TweetFi/tact_TransactionValidator';
+import { TweetFiWallet } from '../build/TweetFi/tact_TweetFiWallet';
 import { buildOnchainMetadata } from "../utils/jetton-helpers";
 import '@ton/test-utils';
 
@@ -10,7 +12,8 @@ describe('TweetFi', () => {
     let admin: SandboxContract<TreasuryContract>;
     let tweetFi: SandboxContract<TweetFi>;
 
-    const merkle_root = "147596663615302291649424969521479109454"
+    const merkle_root = "287443400733757910928349920409317122258"
+    const mint_to_address = Address.parse("EQAX21A4fIw7hX1jmRjvJT0DX7H_FUItj2duCBWtK4ayEiC_")
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
@@ -27,9 +30,9 @@ describe('TweetFi', () => {
         const jetton_content = buildOnchainMetadata(jettonParams);
         const max_supply = toNano("1000000000");
 
-        tweetFi = blockchain.openContract(await TweetFi.fromInit(deployer.address, max_supply, admin.address, jetton_content ));
+        tweetFi = blockchain.openContract(await TweetFi.fromInit(deployer.address, max_supply, admin.address, jetton_content));
 
-        
+
 
         const deployResult = await tweetFi.send(
             deployer.getSender(),
@@ -53,5 +56,201 @@ describe('TweetFi', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and tweetFi are ready to use
+    });
+
+    it('Test: Process', async () => {
+        // the check is done inside beforeEach
+        // blockchain and tweetFi are ready to use
+
+        // test set merkle root
+        const merkle_root_set_res = await tweetFi.send(
+            admin.getSender(),
+            {
+                value: toNano("0.01")
+            },
+            {
+                $$type: 'MerkleRoot',
+                value: merkle_root
+            }
+        )
+
+        expect(merkle_root_set_res.transactions).toHaveTransaction({
+            from: admin.address,
+            to: tweetFi.address,
+            success: true,
+        });
+
+        expect(await tweetFi.getMerkleTreeRoot()).toEqual(merkle_root);
+
+        console.log("Balance:", Number(await tweetFi.getBalance()) / Number(10 ** 9))
+
+        // 1. test mint
+
+        const merkle_root_set_res_of_deployer = await tweetFi.send(
+            deployer.getSender(),
+            {
+                value: toNano("0.01")
+            },
+            {
+                $$type: 'MerkleRoot',
+                value: merkle_root
+            }
+        )
+
+        expect(merkle_root_set_res_of_deployer.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: tweetFi.address,
+            success: false,
+        });
+
+        let cell2: Cell = beginCell()
+            .storeUint(172282571249944562391355093940656328312n, 128)
+            .storeUint(0, 1).endCell();
+
+        let cell1: Cell = beginCell().storeRef(cell2)
+            .storeUint(203179498857370205237139927978231304761n, 128)
+            .storeUint(0, 1).endCell();
+
+        const tweetfi_mint_result = await tweetFi.send(
+            deployer.getSender(),
+            {
+                value: toNano('2'),
+            },
+            {
+                $$type: 'TweetMint',
+                index: 123n,
+                to: Address.parse("EQAX21A4fIw7hX1jmRjvJT0DX7H_FUItj2duCBWtK4ayEiC_"),
+                amount: 10000000000000n,
+                proof: cell1,
+                proof_length: 2n,
+                to_str: "EQAX21A4fIw7hX1jmRjvJT0DX7H_FUItj2duCBWtK4ayEiC_"
+            }
+        )
+
+
+        cell2 = beginCell()
+            .storeUint(172282571249944562391355093940656328312n, 128)
+            .storeUint(0, 1).endCell();
+
+        cell1 = beginCell().storeRef(cell2)
+            .storeUint(145204951486100674980495029225816940097n, 128)
+            .storeUint(1, 1).endCell();
+
+
+        const transaction_validator_address = await tweetFi.getGetTransactionValidatorAddress(123n);
+        const transaction_validator = blockchain.openContract(TransactionValidator.fromAddress(transaction_validator_address));
+
+
+        const deployer_mint_result = await tweetFi.send(
+            deployer.getSender(),
+            {
+                value: toNano('2'),
+            },
+            {
+                $$type: 'TweetMint',
+                index: 1n,
+                to: deployer.address,
+                amount: 990000000000000n,
+                proof: cell1,
+                proof_length: 2n,
+                to_str: "EQBGhqLAZseEqRXz4ByFPTGV7SVMlI4hrbs-Sps_Xzx01x8G"
+            }
+        )
+
+
+        // 1.1 from owner to tweetfi contract
+        expect(tweetfi_mint_result.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: tweetFi.address,
+            success: true,
+        });
+
+        // 1.2 from tweetfi contract to transaction validator contract
+        expect(tweetfi_mint_result.transactions).toHaveTransaction({
+            from: tweetFi.address,
+            to: transaction_validator_address,
+            success: true,
+        });
+
+        console.log("transaction_validator Balance:", Number(await transaction_validator.getBalance()) / 10 ** 9)
+
+        // 1.3 from transaction validator contract to tweetfi contract
+        expect(tweetfi_mint_result.transactions).toHaveTransaction({
+            from: transaction_validator_address,
+            to: tweetFi.address,
+            success: true,
+        });
+
+        console.log("master balance:", await tweetFi.getBalance())
+
+        // 1.4 InternalTweetMint from master to wallet
+        const admin_wallet_address = await tweetFi.getGetWalletAddress(admin.address);
+        const admin_wallet = blockchain.openContract(TweetFiWallet.fromAddress(admin_wallet_address));
+        expect(tweetfi_mint_result.transactions).toHaveTransaction({
+            from: tweetFi.address,
+            to: admin_wallet_address,
+            success: true,
+        });
+
+        console.log("twiffi Balance after InternalTweetMint:", Number(await tweetFi.getBalance()) / 10 ** 9);
+
+        // wallet should be 2000000000000n lock should be 8000000000000n
+        expect((await admin_wallet.getGetWalletData()).balance).toEqual(2000000000000n)
+        expect((await admin_wallet.getLockInfo()).amount).toEqual(8000000000000n)
+
+        const deployer_wallet_address = await tweetFi.getGetWalletAddress(deployer.address);
+        const deployer_wallet = blockchain.openContract(TweetFiWallet.fromAddress(deployer_wallet_address));
+        expect((await deployer_wallet.getGetWalletData()).balance).toEqual(198000000000000n)
+        expect((await deployer_wallet.getLockInfo()).amount).toEqual(792000000000000n)
+       
+
+
+        // 2. test stake
+        const stake_result = await admin_wallet.send(
+            admin.getSender(),
+            {
+                value: toNano('1'),
+            },
+            {
+                $$type: 'Stake',
+                amount: 1000000000000n,
+                inviter: deployer.address,
+            }
+        )
+        expect(stake_result.transactions).toHaveTransaction({
+            from: admin.address,
+            to: admin_wallet_address,
+            success: true,
+        });
+
+        expect((await admin_wallet.getStakeInfo()).amount).toEqual(1000000000000n)
+        expect((await deployer_wallet.getLockInfo()).amount).toEqual(791900000000000n)
+
+
+        // 3. test tip
+        const tip_result = await deployer_wallet.send(
+            deployer.getSender(),
+            {
+                value: toNano("5")
+            },
+            {
+                $$type: 'Tip',
+                query_id: 0n,
+                amount: toNano("50000"),
+                destination: admin.address,
+                response_destination: deployer.address,
+                forward_payload: beginCell().endCell()
+            }
+        )
+
+        expect(tip_result.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: deployer_wallet.address,
+            success: true,
+        });
+
+    
+  
+
     });
 });
