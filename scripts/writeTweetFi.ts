78d784e84fdb07bf36c4445615429b9fa4e25e95
geb <853934146@qq.com>
2024-07-09 00:07:50 +0800

    feat: add mint example

--- scripts/writeTweetFi.ts
+++ scripts/writeTweetFi.ts
@@ -1,7 +1,8 @@
 import { Address, toNano } from '@ton/core';
 import { TweetFi, loadMerkleAdmin } from '../wrappers/TweetFi';
 import { NetworkProvider } from '@ton/blueprint';
-import { buildOnchainMetadata } from "../utils/jetton-helpers";
+import { buildOnchainMetadata, createProofCells } from "../utils/jetton-helpers";
+
 const Sleep = (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
@@ -9,7 +10,7 @@ const Sleep = (ms: number) => {
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    const address = Address.parse("0QCe6-RwMOHP1DR8ZWaOfCvtGhYW8nqij-t65iqQCSK9cA6k");
+    const address = Address.parse("kQAImG1uskTfoiCpiPeBhp0-epIHE36z763cs5RDC9DNgZNW");
 
 
     const tf = provider.open(TweetFi.fromAddress(address));
@@ -34,5 +35,23 @@ export async function run(provider: NetworkProvider, args: string[]) {
 
     const merkle_root = await tf.getMerkleTreeRoot()
     console.log(merkle_root)
+
+    const proof = ['137371635651936680969379878616821974210', '162898467532614113139102293973951447618', '296188890061580092281432174293880712930'];
+    let cell1 = createProofCells(proof)
+
+    await tf.send(
+        provider.sender(),
+        {value: toNano('0.2')},
+        {
+            $$type: "TweetMint",
+            index: 0n,
+            to: Address.parse("UQAEg6xitp3M_Pj9pjHQpLeGZ8PxIEH2RwGCTpMNE6sOjycs"),
+            amount: 10000000000000n,
+            proof: cell1,
+            proof_length: 3n,
+            to_str: "UQAEg6xitp3M_Pj9pjHQpLeGZ8PxIEH2RwGCTpMNE6sOjycs"
+        }
+    )
+
     ui.write('successfully!');
 }
