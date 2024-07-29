89d19f25f787b3f1a5beacbad7cf5b28eebefbd8
geb <853934146@qq.com>
2024-07-29 19:58:23 +0800

    update

---
+++ scripts/readContract.ts
@@ -0,0 +1,29 @@
+import { Address, toNano } from '@ton/core';
+import { TweetFi, loadMerkleAdmin } from '../wrappers/TweetFi';
+import { TweetFiWallet } from '../build/TweetFi/tact_TweetFiWallet';
+import { NetworkProvider } from '@ton/blueprint';
+
+const Sleep = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+export async function run(provider: NetworkProvider, args: string[]) {
+    const ui = provider.ui();
+
+    const address = Address.parse("kQC0koqOyJIjG9IMmG3QjNbseucO9OoW9Zxg6sLzWuRn3WPo");
+
+
+    const tf = provider.open(TweetFi.fromAddress(address));
+
+   
+
+
+    const user_wallet_address = await tf.getGetWalletAddress(Address.parse("0QCkfhhHphlGejkE9Pkq4B17D2MovSRi59NuFOgdARYxzLwL"))
+    const user_wallet = provider.open(TweetFiWallet.fromAddress(user_wallet_address));
+
+    console.log("claim now:", await user_wallet.getClaimAmountNow())
+
+    console.log("staking info:", await user_wallet.getStakeInfo())
+
+    ui.write('successfully!');
+}
--- scripts/writeTweetFi.ts
+++ scripts/writeTweetFi.ts
@@ -1,5 +1,6 @@
 import { Address, toNano } from '@ton/core';
 import { TweetFi, loadMerkleAdmin } from '../wrappers/TweetFi';
+import { TweetFiWallet } from '../build/TweetFi/tact_TweetFiWallet';
 import { NetworkProvider } from '@ton/blueprint';
 import { buildOnchainMetadata, createProofCells } from "../utils/jetton-helpers";
 
@@ -10,7 +11,7 @@ const Sleep = (ms: number) => {
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    const address = Address.parse("kQAImG1uskTfoiCpiPeBhp0-epIHE36z763cs5RDC9DNgZNW");
+    const address = Address.parse("kQC0koqOyJIjG9IMmG3QjNbseucO9OoW9Zxg6sLzWuRn3WPo");
 
 
     const tf = provider.open(TweetFi.fromAddress(address));
@@ -28,30 +29,38 @@ export async function run(provider: NetworkProvider, args: string[]) {
     //     }
     // );
 
-    ui.write('Waiting for send...');
-    await Sleep(10);
+    // ui.write('Waiting for send...');
+    // await Sleep(10);
     const merkle_admin = await tf.getMerkleAdmin()
     console.log(merkle_admin)
 
     const merkle_root = await tf.getMerkleTreeRoot()
     console.log(merkle_root)
 
-    const proof = ['137371635651936680969379878616821974210', '162898467532614113139102293973951447618', '296188890061580092281432174293880712930'];
-    let cell1 = createProofCells(proof)
+    // const proof = ['137371635651936680969379878616821974210', '162898467532614113139102293973951447618', '296188890061580092281432174293880712930'];
+    // let cell1 = createProofCells(proof)
 
-    await tf.send(
-        provider.sender(),
-        {value: toNano('0.2')},
-        {
-            $$type: "TweetMint",
-            index: 0n,
-            to: Address.parse("UQAEg6xitp3M_Pj9pjHQpLeGZ8PxIEH2RwGCTpMNE6sOjycs"),
-            amount: 10000000000000n,
-            proof: cell1,
-            proof_length: 3n,
-            to_str: "UQAEg6xitp3M_Pj9pjHQpLeGZ8PxIEH2RwGCTpMNE6sOjycs"
-        }
-    )
+    // await tf.send(
+    //     provider.sender(),
+    //     {value: toNano('0.2')},
+    //     {
+    //         $$type: "TweetMint",
+    //         index: 0n,
+    //         to: Address.parse("UQAEg6xitp3M_Pj9pjHQpLeGZ8PxIEH2RwGCTpMNE6sOjycs"),
+    //         amount: 10000000000000n,
+    //         proof: cell1,
+    //         proof_length: 3n,
+    //         to_str: "UQAEg6xitp3M_Pj9pjHQpLeGZ8PxIEH2RwGCTpMNE6sOjycs"
+    //     }
+    // )
+
+
+    const user_wallet_address = await tf.getGetWalletAddress(Address.parse("0QCkfhhHphlGejkE9Pkq4B17D2MovSRi59NuFOgdARYxzLwL"))
+    const user_wallet = provider.open(TweetFiWallet.fromAddress(user_wallet_address));
+
+    console.log("claim now:", await user_wallet.getClaimAmountNow())
+
+    console.log("staking info:", await user_wallet.getStakeInfo())
 
     ui.write('successfully!');
 }
